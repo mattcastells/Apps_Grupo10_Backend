1694f8a01ef9b353a4fdb6aa5f56977ea2e5201f
reborafs <reborafs@gmail.com>
2025-09-09 22:16:39 -0300

    fix otp stuff

--- src/main/java/com/uade/ritmofitapi/model/OTP.java
+++ src/main/java/com/uade/ritmofitapi/model/OTP.java
@@ -2,11 +2,13 @@ package com.uade.ritmofitapi.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import java.time.LocalDate;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 @Document(collection = "user_otp")
 public class OTP {
