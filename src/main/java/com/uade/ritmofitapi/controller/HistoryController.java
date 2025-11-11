6f4d545193017ac72d261718b9f33dafda213555
reborafs <reborafs@gmail.com>
2025-11-10 22:55:09 -0300

    fix history controller

--- src/main/java/com/uade/ritmofitapi/controller/HistoryController.java
+++ src/main/java/com/uade/ritmofitapi/controller/HistoryController.java
@@ -2,10 +2,12 @@ package com.uade.ritmofitapi.controller;
 
 import com.uade.ritmofitapi.dto.response.HistoryDetailResponse;
 import com.uade.ritmofitapi.dto.response.HistoryItemResponse;
+import com.uade.ritmofitapi.model.User;
 import com.uade.ritmofitapi.service.HistoryService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,12 +20,13 @@ public class HistoryController {
 
     private final HistoryService historyService;
 
-    @GetMapping("/users/{userId}")
+    @GetMapping("/user")
     public ResponseEntity<List<HistoryItemResponse>> getUserHistory(
-            @PathVariable String userId,
+            Authentication authentication,
             @RequestParam String from,
             @RequestParam String to) {
-        
+        User user = (User) authentication.getPrincipal();
+        String userId = user.getId();
         log.info("Getting history for user {} from {} to {}", userId, from, to);
         
         try {
